93f20e7757030b10cd0137a9d37e6fd092fcd593
Dibeja <l.dibella@gmail.com>
2012-09-13 14:32:29 +0200

    Updated README and plugin usage

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 how_ministash
 =============
 
-A simple "Stash LIke" plugin for EE1.x
+The Ministash Plugin is aimed to give EE1 users the opportunity to use partials and layouts (more info about in this very good article by John D Wells: http://johndwells.com/blog/homegrown-plugin-to-create-template-partials-for-expressionengine)
+
+It's MINI so you have only three funcions:
+
+__SET:__  
+	{exp:ministash:set name="YOURSTASHNAME"}
+  		content
+	{/exp:ministash}
+
+To set a "one time" stash
+
+__APPEND:__  
+	{exp:ministash:append name="YOURSTASHNAME"}
+  		content
+	{/exp:ministash}
+
+To add content at the bottom of a stash
+
+__GET:__  
+	{exp:ministash:get name="YOURSTASHNAME"}
+
+To retrieve the content previously set
+
+__IMPORTANT!__
+In EE1 the pagination is appended at the bottom of the resultset, so you cannot stash it! Sorry folks, it's not my fault :)
+
+__THANKS TO:__
+Mark Croxton: for the wonderful and original Stash plugin
+Max Lazar: for the MX Jumper plugin wich give me a good starting point
+
+Happy templating!
--- pi.ministash.php
+++ pi.ministash.php
@@ -63,8 +63,37 @@ class Ministash {
   {
   ob_start(); 
   ?>
-The Ministash Plugin ....
+The Ministash Plugin is aimed to give EE1 users the opportunity to use partials and layouts (more info about in this very good article by John D Wells: http://johndwells.com/blog/homegrown-plugin-to-create-template-partials-for-expressionengine)
 
+It's MINI so you have only three funcions:
+
+SET:
+{exp:ministash:set name="YOURSTASHNAME"}
+  content
+{/exp:ministash}
+
+To set a "one time" stash
+
+APPEND:
+{exp:ministash:append name="YOURSTASHNAME"}
+  content
+{/exp:ministash}
+
+To add content at the bottom of a stash
+
+GET:
+{exp:ministash:get name="YOURSTASHNAME"}
+
+To retrieve the content previously set
+
+IMPORTANT!
+In EE1 the pagination is appended at the bottom of the resultset, so you cannot stash it! Sorry folks, it's not my fault :)
+
+THANKS TO:
+Mark Croxton: for the wonderful and original Stash plugin
+Max Lazar: for the MX Jumper plugin wich give me a good starting point
+
+Happy templating!
   <?php
   $buffer = ob_get_contents();
 	
